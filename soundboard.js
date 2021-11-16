ef201d79ee737a0953d1e12da29ebf4a113d4d24
wynsmaab <90644023+wynsmaab@users.noreply.github.com>
2021-11-16 15:52:44 -0500

    commit on 'master'

---
+++ soundboard.js
@@ -0,0 +1,11 @@
+function playSound1 () {
+    document.getElementById('play1').play();
+}
+
+function playSound2 () {
+    document.getElementById('play2').play();
+}
+
+function playSound3 () {
+    document.getElementById('play3').play();
+}
